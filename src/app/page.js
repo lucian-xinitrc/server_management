b1b37e2cce0a389b6fde4a5085d1181b50e4a430
lucian-xinitrc <cusmirlucian@gmail.com>
2025-12-27 22:54:48 +0200

    New commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,95 @@
+'use client'
+import { useState, useEffect } from 'react';
 import Image from "next/image";
 
 export default function Home() {
+  const [logged, islogged] = useState(false);
+  const [password, setPassword] = useState("");
+  const [expanded, setExpanded] = useState({});
+  const secretKey = "12021908";
+
+  const toggleCard = (idx) => {
+    setExpanded(prev => ({
+      ...prev,
+      [idx]: !prev[idx],
+    }));
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (password === secretKey) {
+      islogged(true);
+    } else {
+      alert("Wrong key");
+    }
+  };
+
+  const [requests, setRequests] = useState([]);
+  const [selectedIdx, setSelectedIdx] = useState(null); // indexul cardului selectat
+
+  const fetchRequests = async () => {
+    const res = await fetch("https://gethonis.com:8888/api/getOutput", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            "headers": "string",
+            "command": "output"
+          }),
+        });
+        if(res.ok) {
+          alert("Added Succesfully");
+          setRefreshCount(2);
+        } else {
+          alert("Error");
+        }
+
+    };
+    const data = await res.json();
+    setRequests(data);
+  };
+
+
+
+  useEffect(() => {
+    fetchRequests();
+    const interval = setInterval(fetchRequests, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+    <section className="dark:bg-gray-950 bg-white h-screen justify-center py-20">
+      <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">{!logged ? "Lucian Web Services Admin" : ""}</h2>
+      <div className="bg-transparent px-5 py-10 mx-5 sm:mx-[30%] sm:w-auto  rounded-lg">
+      <h2 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">{!logged ? "Login" : "DashBoard"}</h2>
+      {logged == false ? (
+          <form onSubmit={handleLogin} className="mb-5 flex justify-center font-bold">
+
+            <input type="password"
+              placeholder="Introdu parola"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"aria-describedby="helper-text-explanation" className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center transition duration-700 ease-in-out hover:bg-gray-100 hover:text-black dark:hover:bg-white dark:hover:text-black hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 p-6 sm:px-5 sm:w-auto text-[20px] sm:w-2xl w-[150px] focus:outline-none" placeholder="key" />
+            <button type="submit" className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center transition duration-700 ease-in-out hover:bg-gray-100 hover:text-black dark:hover:bg-white dark:hover:text-black hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 p-6 sm:px-5 w-auto sm:w-2xl md:w-[158px] mx-2 text-[20px]">
+              Login
+            </button>
+          </form>
+        ) : ( 
+
+          <div className="w-full h-full bg-black">
+            {requests.map((req, idx) => (
+                                    <p
+                                            key={idx}
+                                            className="hover:underline">
+                                          {req.output}
+                                          </p>
+            ))}
+          </div>
+
+         )
+      }
+      </div>
+    </section>
+    </>
   );
 }
